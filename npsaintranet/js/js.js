0d3303bad8212dad0af19353cb4f33a33f3aeb23
Connesenai <85834580+Connesenai@users.noreply.github.com>
2021-06-28 06:35:09 -0300

    Delete npsaintranet directory

--- npsaintranet/js/js.js
+++
@@ -1,9 +0,0 @@
-(window.onscroll = function() {
-	var header = document.getElementById("myHeader");
-	var sticky = header.offsetTop;
-	if (window.pageYOffset > sticky) {
-		header.classList.add("sticky");
-	} else {
-		header.classList.remove("sticky");
-	}
-});
